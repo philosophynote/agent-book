4ebc757b6d5bc2794b32610ecb8c345ac4f33691
philosophynote <adverdest@gmail.com>
2025-11-08 10:03:34 +0900

    first comit

---
+++ chapter7/1_langfuse_trial.py
@@ -0,0 +1,19 @@
+from langchain.chat_models import init_chat_model
+from langfuse.langchain import CallbackHandler
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# モデルの準備
+llm = init_chat_model(
+    model="us.anthropic.claude-3-7-sonnet-20250219-v1:0",
+    model_provider="bedrock_converse",
+)
+
+# Langfuseのコールバッククラスのインスタンスを生成
+langfuse_handler = CallbackHandler()
+config={"callbacks": [langfuse_handler]}
+
+# 推論を実行
+response = llm.invoke("日本の総理大臣を答えてください", config=config)
+print(response)
---
+++ chapter7/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from chapter7!")
+
+
+if __name__ == "__main__":
+    main()
